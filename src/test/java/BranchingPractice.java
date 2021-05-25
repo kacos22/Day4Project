b2eae3066503502ad498dfe46143cf2cf9003d62
KarlosMartinez <k4martinez98@gmail.com>
2021-05-25 19:24:32 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
-        
+
+        // created new project
+        // enabled version contril
+        //added .gitignore with .idea, target, and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
     }
 }
