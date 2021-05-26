ce1f60d14f2e2aa26f8a5552cbd0f4c98b271d95
KarlosMartinez <k4martinez98@gmail.com>
2021-05-25 21:18:59 -0400

    TC001 has been complete

--- src/test/java/TextCase001.java
+++ src/test/java/TextCase001.java
@@ -1,4 +1,7 @@
 public class TextCase001 {
     //work 1
     //work2
+    //finished this test case ready to push to remote
+    //ready to pull request (permission to merge)
+    //discuss and eventually merge the code
 }
